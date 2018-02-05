6d4624184ce43be2c65f0ad71e676487c45e103a
Kenezke1 <herczeg.kenya2@gmail.com>
2018-02-05 15:34:08 +0100

    Answer and Question .java

--- src/main/java/com/codecool/Answer.java
+++ src/main/java/com/codecool/Answer.java
@@ -1,4 +1,13 @@
 package com.codecool;
 
 public class Answer {
+    Answer[] answers;
+    Answer[] evaluatedAswers;
+    boolean eavaluateAnswerByInput(String input){
+        return true;
+
+    }
+    void addValue(Value value){
+
+    }
 }
--- src/main/java/com/codecool/Question.java
+++ src/main/java/com/codecool/Question.java
@@ -1,4 +1,25 @@
 package com.codecool;
 
 public class Question {
+    String id;
+    String question;
+    Answer answer;
+
+    Question(String id, String question, Answer answer){
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+    String getId(){
+        return id;
+    }
+    String getQuestion(){
+        return question;
+    }
+    Answer getAnswer(){
+        return answer;
+    }
+    boolean getEvaluatedAnswer(){
+        return true;
+    }
 }
