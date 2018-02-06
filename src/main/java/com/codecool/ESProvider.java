c35abd041a377b5e24cd30c70d063adb4b78f52b
Kenezke1 <herczeg.kenya2@gmail.com>
2018-02-06 12:50:48 +0100

    started implementig the ESProvider methods

--- src/main/java/com/codecool/ESProvider.java
+++ src/main/java/com/codecool/ESProvider.java
@@ -1,4 +1,33 @@
 package com.codecool;
 
+import java.util.Map;
+import java.util.Scanner;
+
 public class ESProvider {
+    FactParser factParser;
+    RuleParser ruleParser;
+    Map<Question,Answer> inputMap =
+
+
+    ESProvider(FactParser factParser,RuleParser ruleParser){
+        this.factParser = factParser;
+        this.ruleParser = ruleParser;
+        Scanner scan  = new Scanner(System.in);
+    }
+    void collectAnswers(){
+        while()
+    }
+    boolean getAnswerByQuestion(String questionId){
+        Map<String,Question> temp= ruleParser.repository.questionMap;
+        for(Map.Entry<String,Question> entry : temp.entrySet()){
+            if (entry.getKey().equals(questionId)){
+                return entry.getValue().getEvaluatedAnswer(answer);
+            }
+        }
+
+    }
+    String evaluate(){
+
+    }
+
 }
