3a827f1830d6f65f54bdfe2fb2008187dff8431b
Matt9993 <hriczmate@gmail.com>
2018-02-06 10:53:51 +0100

    Fact class almost ready

--- src/main/java/com/codecool/Fact.java
+++ src/main/java/com/codecool/Fact.java
@@ -1,4 +1,36 @@
 package com.codecool;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Fact {
+
+    private String description;
+    private Map<String,Boolean> evals;
+
+    Fact(String description) {
+        this.description = description;
+        this.evals = new HashMap<String, Boolean>();
+    }
+
+    public void setFactValueById(String id, boolean value) {
+        evals.put(id, value);
+    }
+
+    public Set<String> getIdSet() {
+        return evals.keySet();
+    }
+
+    public boolean getValieById(String input) {
+        return evals.get(input);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Boolean> getEvals() {
+        return evals;
+    }
 }
