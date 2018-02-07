0ddbcc35e5783ff601d39fbf3e03b7f5d23e9076
Kenezke1 <herczeg.kenya2@gmail.com>
2018-02-07 14:59:54 +0100

    imlemented printing and fixed evaluate() function

--- src/main/java/com/codecool/ESProvider.java
+++ src/main/java/com/codecool/ESProvider.java
@@ -7,6 +7,9 @@ public class ESProvider {
     RuleParser ruleParser;
     Map<String, Boolean> inputMap;
     String input;
+    String[] availableNarcotics = new String[9];
+    String[] availableAlcohols = new String[12];
+
 
     Scanner scan  = new Scanner(System.in);
 
@@ -18,10 +21,21 @@ public class ESProvider {
     }
     void collectAnswers(){
        QuestionIterator a = ruleParser.RuleRepository.getIterator();
-
+        getAvaiableValues();
        while(a.hasNext()){
             Question q = a.next();
-            System.out.println(q.getQuestion());
+            if(q.getId().equals("narcotics")) {
+                System.out.println(q.getQuestion());
+                printAvaiable(availableNarcotics);
+            }
+            else if (q.getId().equals("drink")){
+               System.out.println(q.getQuestion());
+               printAvaiable(availableAlcohols);
+                }
+
+            else{
+                System.out.println(q.getQuestion());
+            }
             input = scan.nextLine();
             Boolean bool = getAnswerByQuestion(q.getId());
             inputMap.put(q.getId(),bool);
@@ -56,7 +70,7 @@ public class ESProvider {
                 }
             }
             if(counter > max) {
-                counter = max;
+                max = counter;
                 d = fact.getDescription();
 
             }
@@ -64,5 +78,41 @@ public class ESProvider {
         }
         return d;
     }
+    void getAvaiableValues(){
+        int counter = 0;
+        int j = 0;
+        int k = 0;
+        for(String[] available : ruleParser.values){
 
+            if (counter < 2){
+                for (int i = 0; i< available.length;i++){
+                    availableAlcohols[j+i] = available[i];
+
+                }
+                counter++;
+                j = available.length;
+            }
+
+            else{
+                for (int i = 0; i< available.length;i++){
+                    availableNarcotics[k+i] = available[i];
+
+                }
+                counter++;
+                k = available.length;
+
+            }
+
+        }
+
+    }
+    void printAvaiable(String[] tempArr){
+        for (int i = 0; i < tempArr.length;i++) {
+            if (i == tempArr.length - 1) {
+                System.out.println(tempArr[i]);
+            } else {
+                System.out.print(tempArr[i] + ",");
+            }
+        }
+    }
 }
