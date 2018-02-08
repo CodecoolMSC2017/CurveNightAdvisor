d20b17fa28fff7c59057b2b33ac777d0b109a2ec
Matt9993 <hriczmate@gmail.com>
2018-02-08 10:20:50 +0100

    factParserTest implemented

--- src/test/com/codecool/FactParserTest.java
+++ src/test/com/codecool/FactParserTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FactParserTest {
 
+    FactParser factParserTest = new FactParser();
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void getFactRepository() {
+        FactRepository test = factParserTest.getFactRepository();
+        Fact ozora = test.getIterator().next();
+        assertEquals(true, ozora.getValueById("money"));
+        assertEquals(false, ozora.getValueById("gamble"));
+        assertThrows(NullPointerException.class, ()-> {
+            ozora.getValueById("baromság");
+        });
     }
 }
